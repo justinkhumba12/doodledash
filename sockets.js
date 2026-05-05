1fdeaf1f1404412b764429488f0fd38333217c8b
justinkhumba12 <justinkhumba111@gmail.com>
2026-05-05 20:27:49 +0530

    New

--- sockets.js
+++ sockets.js
@@ -559,7 +559,7 @@ module.exports = (io) => {
                             if (prefix === 'ad') {
                                 await db.query(`UPDATE users SET credits = credits + ?, ad_claims_today = ?, last_ad_claim_time = UTC_TIMESTAMP() WHERE tg_id = ?`, [rewardAmount, newClaimCount, currentUser]);
                             } else {
-                                await db.query(`UPDATE users SET credits = credits + ?, ad2_claims_today = ?, last_ad_claim_time = UTC_TIMESTAMP() WHERE tg_id = ?`, [rewardAmount, newClaimCount, currentUser]);
+                                await db.query(`UPDATE users SET credits = credits + ?, ad2_claims_today = ?, last_ad2_claim_time = UTC_TIMESTAMP() WHERE tg_id = ?`, [rewardAmount, newClaimCount, currentUser]);
                             }
                             success = true; msg = `Reward claimed! +${rewardAmount} Credit`;
                         } else {
@@ -1006,31 +1006,24 @@ module.exports = (io) => {
                     await db.query('UPDATE users SET credits = credits - ? WHERE tg_id = ?', [inkConfig.cost, currentUser]);
                     await redis.hincrbyfloat('user_credits', currentUser, -inkConfig.cost);
                     
-                    // --- APPLYING YOUR EXACT MATH LOGIC ---
                     member.ink_extra = member.ink_extra || {};
                     member.ink_used = member.ink_used || {};
                     
-                    const currentExtra = member.ink_extra[color] || 0;
-                    const currentUsed = member.ink_used[color] || 0;
+                    const baseFreeInk = inkConfig.free;
+                    const currentInkExtra = member.ink_extra[color] || 0;
+                    const currentInkUsed = member.ink_used[color] || 0;
+                    const newlyPurchasedInk = inkConfig.extra;
+
+                    const currentRemaining = baseFreeInk + currentInkExtra - currentInkUsed;
+                    const newRemaining = currentRemaining + newlyPurchasedInk;
+                    const newTotalCapacity = newRemaining + currentInkUsed;
                     
-                    // 1. Calculate remaining ink (e.g., 2500 free + 0 extra - 2490 used = 10 remaining)
-                    const currentRemaining = Math.max(0, inkConfig.free + currentExtra - currentUsed);
-                    
-                    // 2. Add bought ink to the remaining ink (e.g., 10 remaining + 2500 bought = 2510 new remaining)
-                    const boughtInk = inkConfig.extra;
-                    const newRemaining = currentRemaining + boughtInk;
-                    
-                    // 3. Calculate new total capacity (e.g., 2490 used + 2510 new remaining = 5000 total capacity)
-                    const newTotalCapacity = currentUsed + newRemaining;
-                    
-                    // 4. Set the extra ink modifier (e.g., 5000 total - 2500 free base = 2500 extra)
-                    member.ink_extra[color] = newTotalCapacity - inkConfig.free;
+                    member.ink_extra[color] = newTotalCapacity - baseFreeInk;
                     member.ink_buys[color] = buysMade + 1;
 
                     await saveRoom(room);
                     
                     io.to(`room_${currentRoom}`).emit('update_ink_capacity', { user_id: currentUser, extra: member.ink_extra });
-                    // Notice we do NOT reset ink_used here, keeping your exact remaining ink math intact on frontend
                     
                     const userState = await getUserState(currentUser);
                     if (userState) socket.emit('user_update', userState);
